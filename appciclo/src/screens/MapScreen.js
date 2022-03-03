892adae07586ca83d1c5ec5a551527406eb90fcc
XEZBubbah <63682468+XEZBubbah@users.noreply.github.com>
2022-03-03 12:09:03 -0500

    Solución error puglins de babel.config.js

--- appciclo/src/screens/MapScreen.js
+++ appciclo/src/screens/MapScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { SafeAreaView } from "react-navigation";
+import { Text, StyleSheet } from "react-native";
 
 
 const MapScreen = ({ navigation }) => {
-
     return (
         <SafeAreaView forceInset = {{ top: 'always'}}>
-            <Text> Vista de Mapa </Text>
+            <Text style= {{ fontSize: 50 }}> Vista de Mapa </Text>
         </SafeAreaView>
     )
 }
--- appciclo/src/screens/MenUsuarioScreen.js
+++ appciclo/src/screens/MenUsuarioScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { SafeAreaView } from "react-navigation";
-
+import { Text, StyleSheet } from "react-native";
 
 const  MenUsuarioScreen = ({ navigation }) => {
 
     return (
         <SafeAreaView forceInset = {{ top: 'always'}}>
-
+            <Text style= {{ fontSize: 50 }}> Menú Usuario </Text>
         </SafeAreaView>
     )
 }

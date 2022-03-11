ef497cb2688d3b2cb7ceadfe4b598afa13a244e7
XEZBubbah <63682468+XEZBubbah@users.noreply.github.com>
2022-03-10 19:21:36 -0500

    Api News con Native Base

--- appciclo/src/components/News.js
+++ appciclo/src/components/News.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import { View, Text, ActivityIndicator, ScrollView, Image, StyleSheet } from "react-native";
+import { NativeBaseProvider, Box, AspectRatio, Center, Stack, Heading, HStack } from "native-base";
 
 const API_KEY = '0b51069c92a6439ca92cec0af62ad543';
 
@@ -29,6 +30,7 @@ class News extends Component {
                 news: response.articles
             })
             console.log(this.state.news.length) 
+            console.log(response)
         })
         .catch(error =>{
             console.log(error);
@@ -46,9 +48,62 @@ class News extends Component {
                         {
                             this.state.news.map((news, index) => (
                                 <View key={index}>
-                                    <Image source={{ uri: `${news.urlToImage}`}} 
-                                    style= {{height: 100, width: 100, borderRadius: 10}} />
-                                    <Text style={{alignSelf:"auto"}} > {news.title} </Text>
+                                <NativeBaseProvider>
+                                    <Box alignItems="center">
+                                        <Box maxW="80" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" _dark={{
+                                            borderColor: "coolGray.600",
+                                            backgroundColor: "gray.700"
+                                        }} _web={{
+                                                shadow: 2,
+                                                borderWidth: 0
+                                        }} _light={{
+                                            backgroundColor: "gray.50"
+                                        }}>
+                                    <Box>
+                                    <AspectRatio w="100%" ratio={16 / 9}>
+                                        <Image source={{
+                                        uri: `${news.urlToImage}`
+                                    }} alt="image" />
+                                    </AspectRatio>
+                                    <Center bg="violet.500" _dark={{
+                                    bg: "violet.400"
+                                    }} _text={{
+                                    color: "warmGray.50",
+                                    fontWeight: "700",
+                                    fontSize: "xs"
+                                    }} position="absolute" bottom="0" px="3" py="1.5">
+                                        {news.url}
+                                    </Center>
+                                    </Box>
+                                    <Stack p="4" space={3}>
+                                    <Stack space={2}>
+                                        <Heading size="md" ml="-1" >
+                                            {news.title}
+                                        </Heading>
+                                        <Text fontSize="xs" _light={{
+                                        color: "violet.500"
+                                    }} _dark={{
+                                        color: "violet.400"
+                                    }} fontWeight="500" ml="-0.5" mt="-1">
+                                        The Silicon Valley of India.
+                                        </Text>
+                                    </Stack>
+                                    <Text fontWeight="400">
+                                        {news.description}
+                                    </Text>
+                                    <HStack alignItems="center" space={4} justifyContent="space-between">
+                                        <HStack alignItems="center">
+                                        <Text color="coolGray.600" _dark={{
+                                        color: "warmGray.200"
+                                        }} fontWeight="400">
+                                            {news.publishedAt}
+                                        </Text>
+                                        </HStack>
+                                    </HStack>
+                                    </Stack>
+                                </Box>
+                                </Box>
+                                </NativeBaseProvider>
                                 </View>
                             ))
                         }

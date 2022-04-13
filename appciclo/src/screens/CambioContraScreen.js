79b32d96b06b1e9fc2cf67a1f8f8e206bf3493b6
XEZBubbah <63682468+XEZBubbah@users.noreply.github.com>
2022-04-13 12:39:58 -0500

    Metodo cambiar y recuperar contraseña funcional

--- appciclo/src/screens/CambioContraScreen.js
+++ appciclo/src/screens/CambioContraScreen.js
@@ -1,8 +1,49 @@
 import React from "react";
 import { Button, Alert } from "react-native";
-import { NativeBaseProvider, Box, VStack, FormControl, Input, Center, Text} from 'native-base'
+import { NativeBaseProvider, Box, VStack, FormControl, Input, Center} from 'native-base';
+import useAuth from "../hooks/useAuth";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import axios from "axios";
 
-function CambioContraScreen({navigation}) {
+export default function CambioContraScreen({navigation}) {
+
+    const {auth, logout} = useAuth();
+
+    function asingError(err){
+        Alert.alert(
+            'Tenemos un problema', 
+            `${err}`,
+            [
+                {text: 'Ok'}
+            ]
+        );
+    }
+
+    const onUpdate = () => {
+        logout()
+        navigation.navigate('Login')
+    }
+
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: Yup.object(validationSchema()),
+        validateOnChange: false,
+        onSubmit: async (formValue) => {
+            const Usuario = auth.userName;
+            console.log(formValue);
+            console.log('Soy '+ Usuario);
+            axios.post('http://192.168.1.3:5000/userM/changePassword', {...formValue, Usuario})
+            .then(function(response){
+                console.log (response.data.message);
+                onUpdate();
+            }).catch(function(e){
+                var err = Object.values(e.response.data)[0];
+                asingError(err);
+            })
+        }       
+    });
+
     return (
         <NativeBaseProvider>
             <Center w="100%">
@@ -10,26 +51,36 @@ function CambioContraScreen({navigation}) {
                 <VStack space={3} mt="5">
                 <FormControl>
                         <FormControl.Label>Contraseña Actual</FormControl.Label>
-                        <Input type="password"/>
+                        <Input 
+                            type="password"
+                            value={formik.values.Contra_Actual}
+                            onChangeText={(text) => formik.setFieldValue("Contra_Actual", text)}    
+                        />
                     </FormControl>
                     <FormControl>
                         <FormControl.Label>Ingresa la nueva Contraseña</FormControl.Label>
-                        <Input type="password"/>
+                        <Input 
+                            type="password"
+                            value={formik.values.Nueva_Contra1}
+                            onChangeText={(text) => formik.setFieldValue("Nueva_Contra1", text)}   
+                        />
                     </FormControl>
                     <FormControl>
                         <FormControl.Label>Confirmar la nueva Contraseña</FormControl.Label>
-                        <Input type="password"/>
+                        <Input 
+                            type="password"
+                            value={formik.values.Nueva_Contra2}
+                            onChangeText={(text) => formik.setFieldValue("Nueva_Contra2", text)} 
+                        />
                     </FormControl>
                     <FormControl>
                     <Button
                         title="Confirmar Cambio"
                         onPress={() => {    
-                            
-                        }
-                        }
+                            formik.handleSubmit()
+                        }}
                     />
                     </FormControl>
-                
                 </VStack>
             </Box>
         </Center>
@@ -37,5 +88,19 @@ function CambioContraScreen({navigation}) {
     )
 }
 
+function initialValues(){
 
-export default  CambioContraScreen
+    return {
+        Contra_Actual: "",
+        Nueva_Contra1: "",
+        Nueva_Contra2: "",
+    }
+}
+
+function validationSchema(){
+    return {
+        Contra_Actual: Yup.string().required('Este campo es requerido'),
+        Nueva_Contra1: Yup.string().required('Este campo es requerido'),
+        Nueva_Contra2: Yup.string().required('Este campo es requerido'),
+    }
+}
--- appciclo/src/screens/RecuperarContScreen.js
+++ appciclo/src/screens/RecuperarContScreen.js
@@ -1,24 +1,90 @@
 import React from "react";
-import { Button, NativeBaseProvider, Box, VStack, FormControl, Input, Center, Alert, Text, Collapse} from 'native-base'
+import { Alert } from "react-native";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import axios from "axios";
+import { Button, NativeBaseProvider, Box, VStack, FormControl, Input, Center, Text} from 'native-base'
 
 
 
-function RecuperarContScreen({navigation}) {
-    const [show, setShow] = React.useState(false);
+export default function RecuperarContScreen({navigation}) {
+
+    function asingError(err){
+        Alert.alert(
+            'Tenemos un problema', 
+            `${err}`,
+            [
+                {text: 'Ok'}
+            ]
+        );
+    }
+
+    const onUpdate = () => {
+        navigation.navigate('Login')
+    }
+
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: Yup.object(validationSchema()),
+        validateOnChange: false,
+        onSubmit: async (formValue) => {
+            console.log(formValue);
+            axios.post('http://192.168.1.3:5000/userM/restorePassword', {...formValue})
+            .then(function(response){
+                console.log (response.data.message);
+                onUpdate();
+            }).catch(function(e){
+                var err = Object.values(e.response.data)[0];
+                asingError(err);
+            })
+        }       
+    });
+
+
     return (
         <NativeBaseProvider>
             <Center w="100%">
                 <Box safeArea p="2" py="8" w="90%" maxW="290">
                 <VStack space={3} mt="5">
+                <FormControl>
+                        <FormControl.Label>Nombre Usuario</FormControl.Label>
+                        <Text fontSize={"12"} color={"danger.500"}>{formik.errors.Usuario}</Text>
+                        <Input 
+                            autoCapitalize="none"
+                            value={formik.values.Usuario}
+                            onChangeText={(text) => formik.setFieldValue("Usuario", text)} 
+                        />
+                    </FormControl>
                     <FormControl>
                         <FormControl.Label>Ingresa el Email de tu cuenta</FormControl.Label>
-                        <Input />
+                        <Text fontSize={"12"} color={"danger.500"}>{formik.errors.Correo}</Text>
+                        <Input 
+                            value={formik.values.Correo}
+                            onChangeText={(text) => formik.setFieldValue("Correo", text)} 
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormControl.Label>Ingresa la nueva Contraseña</FormControl.Label>
+                        <Text fontSize={"12"} color={"danger.500"}>{formik.errors.Nueva_Contra1}</Text>
+                        <Input 
+                            type="password"
+                            value={formik.values.Nueva_Contra1}
+                            onChangeText={(text) => formik.setFieldValue("Nueva_Contra1", text)}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormControl.Label>Ingresa nuevamente la nueva Contraseña</FormControl.Label>
+                        <Text fontSize={"12"} color={"danger.500"}>{formik.errors.Nueva_Contra2}</Text>
+                        <Input 
+                            type="password"
+                            value={formik.values.Nueva_Contra2}
+                            onChangeText={(text) => formik.setFieldValue("Nueva_Contra2", text)}
+                        />
                     </FormControl>
                 </VStack>
                 <Button mt="2" colorScheme="indigo" onPress={() => {
-                        navigation.navigate('Cambiar Contraseña')
-                    }
-                }
+                        formik.handleSubmit()
+                    }}
                 >
                     Enviar
                 </Button>
@@ -28,5 +94,22 @@ function RecuperarContScreen({navigation}) {
     )
 }
 
-export default  RecuperarContScreen
+function initialValues(){
+    return {
+        Correo: "",
+        Nueva_Contra1: "",
+        Nueva_Contra2: "",
+        Usuario: "",
+    }
+}
+
+function validationSchema(){
+    return {
+        Correo: Yup.string().email("Debe digitar un correo valido").required('Este campo es requerido'),
+        Nueva_Contra1: Yup.string().required('Este campo es requerido'),
+        Nueva_Contra2: Yup.string().required('Este campo es requerido'),
+        Usuario: Yup.string().required('Este campo es requerido'),
+    }
+}
+
 

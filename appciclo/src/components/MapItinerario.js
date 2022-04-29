573ed9e82182ccde3a14087cd43219207e25da67
XEZBubbah <63682468+XEZBubbah@users.noreply.github.com>
2022-04-29 01:30:42 -0500

    Market ubicación usuario

--- appciclo/src/components/MapItinerario.js
+++ appciclo/src/components/MapItinerario.js
@@ -1,13 +1,15 @@
-import React , { useRef } from "react";
+import React , { useRef, useState, useEffect } from "react";
 import MapView, { Marker } from 'react-native-maps';
 import { API_KEY_GOOGLE_MAPS } from '../store/GoogleMaps'
 import MapViewDirections from 'react-native-maps-directions';
-import { NativeBaseProvider,View } from 'native-base';
+import { NativeBaseProvider,View, Button, Box } from 'native-base';
 import useAuth from "../hooks/useAuth";
-import { StyleSheet, Dimensions } from "react-native";
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location'
+import { StyleSheet, Dimensions, Alert } from "react-native";
 
 const Kml_File = 'https://pastebin.com/raw/203B9ixP'
-const height = Dimensions.get('window').height;
+const height = 640;
 
 const styles = StyleSheet.create({
     map: {
@@ -17,14 +19,45 @@ const styles = StyleSheet.create({
 
 export default function MapItinerario() {
 
-    const { puntoi, puntol } = useAuth();
-    const initialValues = { latitude: 7.126844, longitude: -73.118850, longitudeDelta: 0.007, latitudeDelta: 0.007 };
-    console.log("Inicio "+ JSON.stringify(puntoi));
-    console.log("Final "+JSON.stringify(puntol));
+    const { puntoi, puntol } = useAuth();  
+    const [initialValues, setInitialValues ] = useState(null);
+    const [marketUsuario, setMarketUsuario] = useState(false);
+    console.log("Inicio "+ JSON.stringify(initialValues));
     const partida = { latitude: puntoi.latitude, longitude: puntoi.longitud };
     const llegada = { latitude: puntol.latitude, longitude: puntol.longitud };
-
     const mapRef = useRef()
+    const [ubiUsuario, setUbiUsuario] = useState(
+        { latitude: 7.126844, longitude: -73.118850, longitudeDelta: 0.04, latitudeDelta: 0.04 }
+    );
+
+    const getLocationUser = async() => {
+        const res = {status: false, location: null}
+        const resulPermision = await Location.requestForegroundPermissionsAsync()
+        if (resulPermision.status == "danied") {
+            Alert.alert("Debe dar permisos de Localización");
+            return res
+        }
+        const position = await Location.getCurrentPositionAsync({});
+        const location = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.007,
+            longitudeDelta: 0.007
+        }
+        res.status = true
+        res.location = location
+        setUbiUsuario(res.location);
+        return res
+    }
+    function inciarRuta(){
+        getLocationUser();
+        setMarketUsuario(true);
+    }
+
+    useEffect(() =>{
+        console.log("Soy UbiUsuario "+ JSON.stringify(ubiUsuario));
+        setInitialValues(ubiUsuario);
+    }, [ubiUsuario])
 
     return (
     <NativeBaseProvider>
@@ -35,7 +68,17 @@ export default function MapItinerario() {
             loadingEnabled = {true}
             kmlSrc = {Kml_File} 
             initialRegion = {initialValues}
-        > 
+        >
+        {
+            marketUsuario === true ? (
+            <Marker 
+                image={require('../imgs/user.png')}
+                coordinate={initialValues}
+            /> 
+            ) : (
+                console.log('Hola :)')
+            )
+        }
         <Marker 
             image={require('../imgs/cycling.png')}
             coordinate={partida}
@@ -63,6 +106,14 @@ export default function MapItinerario() {
                 }}
             />        
         </MapView>
+        <View height={200} width= {350} alignSelf= {'center'}>
+            <Box>
+                <Button onPress={inciarRuta}>Iniciar Ruta</Button>
+            </Box>
+            <Box paddingTop={3}>
+                <Button>Enviar Alerta</Button>
+            </Box>
+        </View>
     </View>
     </NativeBaseProvider>
     )
